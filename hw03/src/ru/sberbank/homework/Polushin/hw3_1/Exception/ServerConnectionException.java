e3b33dd917b9a2ae6e37a5989e0f424482114506
ferty <polushin1992@gmail.com>
2018-03-20 19:14:19 +0300

    hw3_1 is done

---
+++ hw03/src/ru/sberbank/homework/Polushin/hw3_1/Exception/ServerConnectionException.java
@@ -0,0 +1,7 @@
+package ru.sberbank.homework.Polushin.hw3_1.Exception;
+
+public class ServerConnectionException extends Exception {
+    public ServerConnectionException(String message) {
+        super(message);
+    }
+}
---
+++ hw03/src/ru/sberbank/homework/Polushin/hw3_1/Utils/Operations.java
@@ -0,0 +1,40 @@
+package ru.sberbank.homework.Polushin.hw3_1.Utils;
+
+
+public enum Operations {
+    GET(1) {
+        @Override
+        public String evoke(Money money) {
+
+            return super.evoke(money);
+        }
+    },
+    TAKE(2) {
+        @Override
+        public String evoke(Money money) {
+            return super.evoke(money);
+        }
+    },
+    CHECK(3) {
+        @Override
+        public String evoke(Money money) {
+            return super.evoke(money);
+        }
+    },
+    SET(4) {
+        @Override
+        public String evoke(Money money) {
+            return super.evoke(money);
+        }
+    };
+
+    private int code;
+
+    Operations(int code) {
+        this.code = code;
+    }
+
+    public String evoke(Money money) {
+        return "Done";
+    }
+}
