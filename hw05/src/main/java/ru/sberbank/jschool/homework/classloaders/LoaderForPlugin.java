5663d143606d721068e8b1beca2676c9b066436a
ferty <polushin1992@gmail.com>
2018-03-25 15:33:11 +0300

    Done hw 5.1

---
+++ hw05/src/main/java/ru/sberbank/jschool/homework/classloaders/LoaderForPlugin.java
@@ -0,0 +1,143 @@
+package ru.sberbank.jschool.homework.classloaders;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.*;
+
+public class LoaderForPlugin extends ClassLoader {
+    private Map<String, Class<?>> classLoaded = new HashMap();
+    private ClassLoader parent;
+
+    public LoaderForPlugin(ClassLoader parent) {
+        super(parent);
+        this.parent = parent;
+    }
+
+    public Map<String, Class<?>> getClassLoaded() {
+        return classLoaded;
+    }
+
+    @Override
+    public Class<?> loadClass(String className) throws ClassNotFoundException {
+        synchronized (getClassLoadingLock(className)) {
+            // First, check if the class has already been loaded
+            Class<?> c = isLoaded(className);
+
+            /*
+            Делегирование родительскому загрузчику. Необходимо, т.к. нужно найти
+            загруженный класс самого интерфейса!
+             */
+            if (c == null) {
+                long t0 = System.nanoTime();
+                try {
+                    if (parent != null) {
+                        c = parent.loadClass(className);
+                    }
+                } catch (ClassNotFoundException e) {
+                    // ClassNotFoundException thrown if class not found
+                    // from the non-null parent class loader
+                }
+
+                if (c == null) {
+                    // If still not found, then invoke findClass in order
+                    // to find the class.
+                    long t1 = System.nanoTime();
+                    c = findClass(className);
+
+                    // this is the defining class loader; record the stats
+                    sun.misc.PerfCounter.getParentDelegationTime().addTime(t1 - t0);
+                    sun.misc.PerfCounter.getFindClassTime().addElapsedTimeFrom(t1);
+                    sun.misc.PerfCounter.getFindClasses().increment();
+                    classLoaded.put(className, c);
+                }
+            }
+            return c;
+        }
+    }
+
+    private Class<?> isLoaded(String className) {
+        for (Map.Entry<String, Class<?>> instance : classLoaded.entrySet()) {
+            if (instance.getKey().equals(className)) {
+                return instance.getValue();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected Class<?> findClass(String className) throws ClassNotFoundException {
+        try {
+            /*
+              Получем байт-код из файла и загружаем класс в рантайм
+             */
+            File file = new File(className);
+            String shortname = file.getName().split(".class$")[0];
+            byte b[] = fetchClassFromFS(file);
+
+            /*
+            Это КОСТЫЛЬ. Не знаю как обходить ситуацию, когда у плагина пользователя
+            есть package. Сделал так, что имя пакета берется из сообщения об ощибке,
+            т.к. оно сожержит правильное имя взятое из считывания байт кода файла.
+             */
+            while (true) {
+                try {
+                    return defineClass(shortname, b, 0, b.length);
+                } catch (NoClassDefFoundError e) {
+                    String message = e.getMessage();
+                    shortname = message.split("wrong name: ")[1];
+                    shortname = shortname.substring(0, shortname.length() - 1)
+                            .replace("/", ".");
+                } catch (LinkageError e) {
+                    /*
+                    Обработка классов с одинаковыми именами. Просто создаем новый загрузчик родителем
+                    которого является текущий.
+                     */
+                    return new LoaderForPlugin(this).defineClass(shortname, b, 0, b.length);
+                }
+            }
+        } catch (FileNotFoundException ex) {
+            return super.findClass(className);
+        } catch (IOException ex) {
+            return super.findClass(className);
+        }
+    }
+
+    /**
+     * Взято из www.java-tips.org/java-se-tips/java.io/reading-a-file-into-a-byte-array.html
+     * Считывает байт код файла.
+     */
+    private byte[] fetchClassFromFS(File file) throws FileNotFoundException, IOException {
+
+        FileInputStream is = new FileInputStream(file);
+
+        // Get the size of the file
+        long length = file.length();
+
+        if (length > Integer.MAX_VALUE) {
+            // File is too large
+        }
+
+        // Create the byte array to hold the data
+        byte[] bytes = new byte[(int) length];
+
+        // Read in the bytes
+        int offset = 0;
+        int numRead = 0;
+        while (offset < bytes.length
+                && (numRead = is.read(bytes, offset, bytes.length - offset)) >= 0) {
+            offset += numRead;
+        }
+
+        // Ensure all the bytes have been read in
+        if (offset < bytes.length) {
+            throw new IOException("Could not completely read file " + file.getName());
+        }
+
+        // Close the input stream and return bytes
+        is.close();
+        return bytes;
+
+    }
+}
--- hw05/src/main/java/ru/sberbank/jschool/homework/classloaders/PluginManager.java
+++ hw05/src/main/java/ru/sberbank/jschool/homework/classloaders/PluginManager.java
@@ -1,9 +1,15 @@
 package ru.sberbank.jschool.homework.classloaders;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PluginManager {
 
     // directory that contains plugin folders
     private final String rootDirectory;
+    LoaderForPlugin loader = new LoaderForPlugin(this.getClass().getClassLoader());
+    private List<Class> loaded = new ArrayList<>();
 
     public PluginManager(String rootDirectory) {
         this.rootDirectory = rootDirectory;
@@ -11,16 +17,80 @@ public class PluginManager {
 
     /**
      * method takes as a parameter a folder name in the root plugin directory,
-     * loads the plugin .class file from the folder if present,
+     * loads the plugin .class files from the folder if present,
      * and returns a Plugin object
      *
      * @param pluginName - name of the plugin folder
-     * @return Plugin
+     * @return Plugin[]
      * @throws PluginNotFoundException - when folder named 'pluginName' is missing,
-     *                                   or it contains no .class files
+     *                                 or it contains no .class files
      */
-    public Plugin loadPlugin(String pluginName) throws PluginNotFoundException {
-        //TODO implement
-        throw new PluginNotFoundException("couldn't locate plugin " + pluginName);
+    public List<Plugin> loadPlugin(String pluginName) throws PluginNotFoundException {
+        List<Plugin> loadedPlugins = new ArrayList<>();
+        Plugin plugin;
+        boolean findFlag = false;
+
+
+        File directories = new File(rootDirectory);
+        for (File directory : directories.listFiles()) {
+
+                /*
+                Ищем нашу папку с плагинами: если это директория и ее имя совпадает
+                 с именем плагина, введеным пользователем, то пытаемся найти .class файлы,
+                иначе кидаем исключение.
+                 */
+            if (directory.isDirectory() && directory.getName().equals(pluginName)) {
+
+                directories = new File(rootDirectory + "\\" + pluginName);
+
+                    /*
+                    Подгружаем все .class файлы или кидаем исключение если таких нет
+                     */
+                for (File file : directories.listFiles()) {
+                        /*
+                        Проверяем что текущий экземпляр является файлом и его расширение .class
+                         */
+                    if (file.isFile() && file.getName().endsWith(".class")) {
+                        try {
+                            Class clazz = loader.loadClass(file.getAbsolutePath());
+
+                            /*
+                            Проверка уже загруженных плагинов
+                             */
+                            if (!loaded.contains(clazz)) {
+                                plugin = (Plugin) clazz.newInstance();
+                                loaded.add(clazz);
+                                loadedPlugins.add(plugin);
+                            }
+                            findFlag = true;
+
+                        } catch (ClassNotFoundException e) {
+                            throw new PluginNotFoundException(
+                                    e.getMessage() + " couldn't locate plugin " + pluginName);
+                        } catch (InstantiationException e) {
+                            throw new PluginNotFoundException(
+                                    e.getMessage() + " couldn't locate plugin " + pluginName);
+                        } catch (IllegalAccessException e) {
+                            throw new PluginNotFoundException(
+                                    e.getMessage() + " couldn't locate plugin " + pluginName);
+                        }
+                    }
+                }
+                break; //Выходим из цикла если нашли нужную директорию и загрузили все .class файлы.
+            }
+
+        }
+        /*
+         Бросаем исключение если не смогли подгрузить плагин, или возращаем список загруженных файлов
+        */
+        if (findFlag == false) {
+            throw new PluginNotFoundException("couldn't locate plugin " + pluginName);
+        } else {
+
+            return loadedPlugins;
+        }
+
     }
+
+
 }
