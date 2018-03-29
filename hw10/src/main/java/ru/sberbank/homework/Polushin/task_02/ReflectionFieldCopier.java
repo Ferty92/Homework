7972c80811bdbc0d2ea8fadb858ce22692d58df0
ferty <polushin1992@gmail.com>
2018-03-30 00:16:32 +0300

    Done task_2

--- hw10/src/main/java/ru/sberbank/homework/Polushin/task_02/ReflectionFieldCopier.java
+++ hw10/src/main/java/ru/sberbank/homework/Polushin/task_02/ReflectionFieldCopier.java
@@ -1,10 +1,53 @@
 package ru.sberbank.homework.Polushin.task_02;
 
+import org.apache.commons.lang3.reflect.TypeUtils;
 import ru.sberbank.homework.common.BeanFieldCopier;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ReflectionFieldCopier implements BeanFieldCopier {
     @Override
     public void copy(Object from, Object to) {
-
+        List<String> methodsFrom = Arrays.stream(from.getClass().getMethods())
+                .filter(element -> element.getName().contains("get"))
+                .map(Method::getName)
+                .collect(Collectors.toList());
+//        List<String> methodsTo = Arrays.stream(to.getClass().getMethods())
+//                .filter(element -> element.getName().contains("set"))
+//                .map(Method::getName)
+//                .collect(Collectors.toList());
+        
+        for (String get : methodsFrom) {
+            
+            try {
+                Method fromMethod = from.getClass().getMethod(get);
+                
+                String toSetMethod = get.replace("get", "set");
+                
+                for (Method toMethod : to.getClass().getMethods()) {
+                    if (toMethod.getName().contains(toSetMethod)) {
+                        Type typeRetFrom = from.getClass().getMethod(get).getGenericReturnType();
+                        Type typeGetTo = toMethod.getGenericParameterTypes()[0];
+                        
+                        /*
+                        Проверяем не только сам тип поля, но и параметр типа.
+                        После чего можем вызывать метод сет объекта потребителя и внего передавать
+                         значение полученное из метода гет обекта продюсера
+                         */
+                        if (TypeUtils.isAssignable(typeRetFrom, typeGetTo)) {
+                            toMethod.invoke(to, fromMethod.invoke(from));
+                        }
+                    }
+                }
+                
+            } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
---
+++ hw10/src/test/java/ru/sberbank/homework/Polushin/task_02/ReflectionFieldCopierTest.java
@@ -0,0 +1,49 @@
+package ru.sberbank.homework.Polushin.task_02;
+
+import org.junit.Before;
+import org.junit.Test;
+import ru.sberbank.homework.Polushin.task_02.resources.First;
+import ru.sberbank.homework.Polushin.task_02.resources.Second;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+public class ReflectionFieldCopierTest {
+    private First first;
+    private Second second;
+    private ReflectionFieldCopier copier;
+    
+    @Before
+    public void setUp() throws Exception {
+        copier = new ReflectionFieldCopier();
+        Set<String> stringSet = new HashSet<>(Arrays.asList("One", "ONE", "one"));
+        Set<Integer> integerSet = new HashSet<>(Arrays.asList(2, 22, 222));
+        first = new First(1, 100, 0.1, "hey first", Arrays.asList(1, 2, 3), stringSet);
+        second = new Second(2, 200L, 0.2, "ho second", integerSet);
+    }
+    
+    @Test
+    public void copyFirstSecond() {
+        copier.copy(first, second);
+        System.out.println(first);
+        System.out.println(second);
+        assertEquals(first.getI(), second.getI());
+        assertEquals(first.getL(), second.getL());
+        assertEquals(first.getString(), second.getString());
+        assertEquals(first.getD(), second.getD(), 1E-10);
+    }
+    
+    @Test
+    public void copySecondFirst() {
+        copier.copy(second, first);
+        System.out.println(first);
+        System.out.println(second);
+        assertEquals(first.getI(), second.getI());
+        assertNotEquals(first.getL(), second.getL());
+        assertEquals(first.getString(), second.getString());
+        assertEquals(first.getD(), second.getD(), 1E-12);
+    }
+}
